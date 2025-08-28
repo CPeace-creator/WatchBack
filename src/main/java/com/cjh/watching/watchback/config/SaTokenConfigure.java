88fa26eec5b2a1e597ab123324226dcf1d204fd4
cjh <949661474@qq.com>
2025-08-28 15:18:22 +0800

    修改问题

--- src/main/java/com/cjh/watching/watchback/config/SaTokenConfigure.java
+++ src/main/java/com/cjh/watching/watchback/config/SaTokenConfigure.java
@@ -14,6 +14,6 @@ public class SaTokenConfigure implements WebMvcConfigurer {
         // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
         registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
                 .addPathPatterns("/**")
-                .excludePathPatterns("/auth/login","auth/register").excludePathPatterns("/error");
+                .excludePathPatterns("/auth/login", "/auth/register", "/error");
     }
 }
